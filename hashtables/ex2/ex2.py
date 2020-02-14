6c521f94adf7dce0060cb7e92f1d5ff02ec2036a
karenjli <karenjy.li@gmail.com>
2020-02-14 10:57:43 -0600

    Hashtable ex1 completed

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -5,6 +5,8 @@ from hashtables import (HashTable,
                         hash_table_retrieve,
                         hash_table_resize)
 
+import json
+
 
 class Ticket:
     def __init__(self, source, destination):
@@ -15,9 +17,13 @@ class Ticket:
 def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
+    
 
-    """
-    YOUR CODE HERE
-    """
+    
+ticket_1 = Ticket("NONE", "PDX")
+ticket_2 = Ticket("PDX", "DCA")
+ticket_3 = Ticket("DCA", "NONE")
 
-    pass
+tickets = [ticket_1, ticket_2, ticket_3]
+
+print(reconstruct_trip(tickets, 3))
